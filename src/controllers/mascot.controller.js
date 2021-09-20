628470217d9d23311b2d0c1676244917f4fbaf55
mBaenaCh <mateo.baena@hotmail.com>
2021-09-19 21:04:44 -0500

    Controller definition

---
+++ src/controllers/mascot.controller.js
@@ -0,0 +1,21 @@
+
+
+export const createMascot = async (req, res) => {
+    
+};
+
+export const getMascots = async (_, res) => {
+
+};
+
+export const getMascotById = async (req, res) => {
+
+};
+
+export const updateMascotById = async (req, res) => {
+
+};
+
+export const deleteMascotById = async (req,res) => {
+    
+}
