ccbba53fb8404801f68ede209f482d53a23c009b
mBaenaCh <mateo.baena@hotmail.com>
2021-09-19 22:39:48 -0500

    last mascot service changes

--- src/controllers/mascot.controller.js
+++ src/controllers/mascot.controller.js
@@ -1,21 +1,58 @@
+const mascotService = require('../services/mascot.service');
 
-
-export const createMascot = async (req, res) => {
-    
+const createMascot = async (req, res) => {
+    try {
+        const mascot = req.body;
+        const createdMascot = await mascotService.createMascotService(mascot);
+        return res.status(201).json(createdMascot);
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({message: 'Lo sentimos, ha ocurrido un problema'});
+    }
 };
 
-export const getMascots = async (_, res) => {
-
+const getMascots = async (_, res) => {
+    try {
+        const mascots = await mascotService.getMascotsService();
+        return res.status(200).json(mascots);
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({message: 'Lo sentimos, ha ocurrido un problema'});
+    }
 };
 
-export const getMascotById = async (req, res) => {
-
+const getMascotById = async (req, res) => {
+    try {
+        const { mascotId } = req.params;
+        const mascot = await mascotService.getMascotService(mascotId);
+        return res.status(200).json(mascot);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: 'Lo sentimos, ha ocurrido un problema'});
+    }
 };
 
-export const updateMascotById = async (req, res) => {
-
+const updateMascotById = async (req, res) => {
+    try {
+        const { mascotId } = req.params;
+        const mascot = req.body;
+        const updatedMascot = await mascotService.updateMascotService(mascotId, mascot);
+        return res.status(200).json(updatedMascot);
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({message: 'Lo sentimos, ha ocurrido un problema'});
+    }
 };
 
-export const deleteMascotById = async (req,res) => {
+const deleteMascotById = async (req,res) => {
+    try {
+        const {mascotId} = req.params;
+        await mascotService.deleteMascotService(mascotId);
+        return res.status(200).json({message: 'La mascota ha sido eliminada'});
+    } catch(error){
+        console.log(error);
+        return res.statu(500).json({message: 'Lo sentimos, ha ocurrido un problema'});
+    }
+};
 
-}
+module.exports = { createMascot, getMascots, getMascotById, updateMascotById, deleteMascotById}
